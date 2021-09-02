aa06e091126ce28a6cb5951ed348c14fcb07450a
Pankaj Mishra <88949597+aipankajmishra@users.noreply.github.com>
2021-09-02 13:54:19 +0200

    comments

--- directory_creator.sh
+++ directory_creator.sh
@@ -1,4 +1,8 @@
 #Creates sub-directory in "Kaggle-30-days-ML" folder of specific pattern(Day1,Day2,...,Day 30")
+#Script has to be on the same level as of the Parent directory i.e "Kaggle-30-days-ML", and the directory should exist.
+# Few checks can be added to make the parent directory as well if that does not exist
+
+
 for x in {1..31}
 do
 	mkdir "Kaggle-30-days-ML/Day${x}"
